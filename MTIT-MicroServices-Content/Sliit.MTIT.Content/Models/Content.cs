4718c37bbadb038915f21d0420b6a0485a57a033
joshuaamarasinghe <joshuaamarasinghe@gmail.com>
2023-04-18 23:02:09 +0530

    initial commit

---
+++ MTIT-MicroServices-Content/Sliit.MTIT.Content/Models/Content.cs
@@ -0,0 +1,10 @@
+﻿namespace Sliit.MTIT.Content.Models
+{
+    public class Content
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public int Age { get; set; }
+    }
+}
