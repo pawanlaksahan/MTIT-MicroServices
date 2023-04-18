4e2d478a6fbd08bd9c9d019fb714a197827a7e80
pawanlaksahan <pawanlaksahan10@gmail.com>
2023-04-18 20:55:13 +0530

    customer microservice

---
+++ Sliit.MTIT.Customer/Data/CustomerMockDataService.cs
@@ -0,0 +1,16 @@
+﻿namespace Sliit.MTIT.Customer.Data
+{
+    public class CustomerMockDataService
+    {
+
+        public static List<Models.Customer> Customers = new List<Models.Customer>()
+        {
+            new Models.Customer { Id = 1, Name = "John", Address = "123 Main St", Age = 20 },
+            new Models.Customer { Id = 2, Name = "Mary", Address = "456 Second Ave", Age = 22 },
+            new Models.Customer { Id = 3, Name = "Tom", Address = "789 Third St", Age = 21 },
+            new Models.Customer { Id = 4, Name = "Kate", Address = "321 Fourth Ave", Age = 23 },
+            new Models.Customer { Id = 5, Name = "Mike", Address = "654 Fifth St", Age = 19 }
+        };
+
+    }
+}
