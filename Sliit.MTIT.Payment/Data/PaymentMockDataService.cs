26b022b5a3f9ca09578cb1103ecf208b7347669b
ITharu123 <tharushiranaweera2071@gmail.com>
2023-04-22 10:35:05 +0530

    change cvc

--- Sliit.MTIT.Payment/Data/PaymentMockDataService.cs
+++ Sliit.MTIT.Payment/Data/PaymentMockDataService.cs
@@ -11,7 +11,7 @@ namespace Sliit.MTIT.Payment.Data
             new Models.Payment { Id = 2, CardNumber = 997788678, ExpiryDate = "2025/09/10", CVC = 344, Amount = 9000 },
             new Models.Payment { Id = 3, CardNumber = 778899075, ExpiryDate = "2024/05/20", CVC = 455, Amount = 8000 },
             new Models.Payment { Id = 4, CardNumber = 778899543, ExpiryDate = "2023/08/15", CVC = 677, Amount = 5000 },
-            new Models.Payment { Id = 5, CardNumber = 335566889, ExpiryDate = "2025/05/29", CVC = 555, Amount = 12000 }
+            new Models.Payment { Id = 5, CardNumber = 335566889, ExpiryDate = "2025/05/29", CVC = 888, Amount = 12000 }
 
         };
 
