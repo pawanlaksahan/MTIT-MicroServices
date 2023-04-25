a6508b0bd312b635e2a1145fafc37aa6f32c58cf
joshuaamarasinghe <joshuaamarasinghe@gmail.com>
2023-04-25 16:26:09 +0530

    file deletion

--- README.md
+++
@@ -1,2 +0,0 @@
-# MTIT-MicroServices
-Assignment 03 
--- Sliit.MTIT.Ocelot/Program.cs
+++
@@ -1,6 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Welcome to the Payment Microservice");
-
-app.Run();
