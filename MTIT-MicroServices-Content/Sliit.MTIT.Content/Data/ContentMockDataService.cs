c30ee61b9d0f3ddab63f6b90d590a16444b9f7a2
joshuaamarasinghe <joshuaamarasinghe@gmail.com>
2023-04-25 13:59:20 +0530

    commit

--- MTIT-MicroServices-Content/Sliit.MTIT.Content/Data/ContentMockDataService.cs
+++ MTIT-MicroServices-Content/Sliit.MTIT.Content/Data/ContentMockDataService.cs
@@ -5,11 +5,11 @@
 
         public static List<Models.Content> Contents = new List<Models.Content>()
         {
-            new Models.Content { Id = 1, ProviderName = "John", Broadcast = "123 Main St", Country = "20", Description = "20", Result = "20" },
-            new Models.Content { Id = 2, ProviderName = "Mary", Broadcast = "456 Second Ave", Country = "22", Description = "20", Result = "20"  },
-            new Models.Content { Id = 3, ProviderName = "Tom", Broadcast = "789 Third St", Country = "21" , Description = "20", Result = "20" },
-            new Models.Content { Id = 4, ProviderName = "Kate", Broadcast = "321 Fourth Ave", Country = "23", Description = "20", Result = "20"  },
-            new Models.Content { Id = 5, ProviderName = "Mike", Broadcast = "654 Fifth St", Country = "19", Description = "20", Result = "20"  }
+            new Models.Content { Id = 1, ProviderName = "Nilusha", Broadcast = "NBA ", Country = "England", Description = "LA Lakers are 2-4 ATS in their last 6 games.", Result = "2-4" },
+            new Models.Content { Id = 2, ProviderName = "Krishan", Broadcast = "MLB", Country = "UK", Description = "The total has gone OVER in 6 of Miami's last 6 games when playing on the road against Milwaukee.", Result = "6-0"  },
+            new Models.Content { Id = 3, ProviderName = "Sandaka", Broadcast = "NHL", Country = "England" , Description = "Philadelphia are 14-3 SU in their last 17 games against an opponent in the Eastern Conference conference.", Result = "14-3" },
+            new Models.Content { Id = 4, ProviderName = "Udaya", Broadcast = "NCAAB", Country = "Australia", Description = "Minnesota are 11-5 ATS in their last 16 games played on a Wednesday.", Result = "11-5"  },
+            new Models.Content { Id = 5, ProviderName = "Edmond", Broadcast = "UFC", Country = "UK", Description = "Miami are 8-2 SU in their last 10 games played in April.", Result = "8-2"  }
         };
 
     }
